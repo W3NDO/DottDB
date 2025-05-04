d8cb3b87c53d86d80df23b8b05ae14af78f6a220
W3NDO <wendonyang@gmail.com>
2025-05-04 19:41:53 +0300

    chore(features/triples)  -added more test casesy

--- test/query_test.exs
+++ test/query_test.exs
@@ -25,5 +25,20 @@ defmodule QueryTest do
       assert {:ok, [%Types.Triples{subject: :anna, predicate: :knows, object: :camille}]} =
                Query.query(graph, [:anna, :__var, :__var])
     end
+
+    test "use no literals and blanks only", %{graph: graph} do
+      assert {:ok,
+      [
+        %Types.Triples{subject: :camille, predicate: :knows, object: :anna},
+        %Types.Triples{subject: :anna, predicate: :knows, object: :camille}
+      ]} = Query.query(graph, [:__var, :__var, :__var])
+    end
+
+    test "use one blank and 2 literals", %{graph: graph} do
+      assert {:ok,
+      [
+        %Types.Triples{subject: :anna, predicate: :knows, object: :camille}
+      ]} = Query.query(graph, [:__var, :knows, :camille])
+    end
   end
 end
